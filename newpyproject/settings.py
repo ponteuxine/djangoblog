20d5b623b6551fe3790d77b3d812ea779644b5b3
Neo <nelsonvan321@gmail.com>
2017-05-15 00:35:50 +0300

    Added urls etc

--- newpyproject/settings.py
+++ newpyproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p9n28n$avnjh3y_mmohc&8r&=h%dz#85&+%j!&0bz*275vuf9y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nelsonvan.pythonanywhere.com']
 
 
 # Application definition
